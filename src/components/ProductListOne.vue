1efe814067aa02a8614122ac4c0128ec21115fc1
Ishan Manandhar <ishan02016@gmail.com>
2018-03-13 16:58:32 +0545

    some changes

---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Product List One</h1>
+    <br><br>
+    <h5>List of products:</h5>
+    <ul>
+      <li v-for="product in reduceStock">
+        <span>{{product.name}}</span>
+        <span>${{product.price}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductListOne',
+  computed: {
+    products(){
+      return this.$store.state.products;
+    },
+    reduceStock(){
+      var reduceStock = this.$store.state.products.map(products => {
+        return {
+          name:'##'+ product name + '##',
+          price: product price/3
+        }
+      });
+      return reduceStock;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
